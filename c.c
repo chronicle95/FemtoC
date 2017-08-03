fae3aa1888f2d26d39f75280b69cb5534c00f7bf
Artem Bondarenko <artembondarenko@i.ua>
2017-08-03 07:15:03 +0300

    Populated the code a bit further

--- c.c
+++ c.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+/* Character test functions */
+
 int is_space(char c)
 {
-	return (c == ' ' || c == '\t'
-	      c == '\n' || c == '\r');
+	return (c == ' ' || c == 10
+			c == 13  || c == 8);
 }
 
 int is_digit(char c)
@@ -14,22 +16,24 @@ int is_digit(char c)
 int is_id0(char c)
 {
 	return (c == '_'
-	  || (c >= 'A' && c <= 'Z')
-	  || (c >= 'a' && c <= 'z'));
+			|| (c >= 'A' && c <= 'Z')
+			|| (c >= 'a' && c <= 'z'));
 }
 
 int is_id(char c)
 {
 	return is_id0 (c)
-           || is_digit (c);
+		|| is_digit (c);
 }
+
+/* Read functions */
 
 int read_space(char *p, int *i)
 {
 	while (is_space (*(p+*i)))
-    {
-        *i = *i + 1;
-    }
+	{
+		*i = *i + 1;
+	}
 	return 1;
 }
 
@@ -47,33 +51,104 @@ int read_sym(char *p, int *i, char exp)
 int read_id(char *p, int *i, char *dst)
 {
 	read_space (p, i);
-	if (!is_id0 (*p))
-    {
-        return 0;
-    }
+	if (!is_id0 (*(p+*i)))
+	{
+		return 0;
+	}
 	while (is_id (*(p+*i)))
-    {
-    	*dst = *(p+*i);
-        dst = dst + 1;
-        *i = *i + 1;
-    }
-    *dst = 0;
-    return 1;
+	{
+		*dst = *(p+*i);
+		dst = dst + 1;
+		*i = *i + 1;
+	}
+	*dst = 0;
+	return 1;
 }
 
 int read_number(char *p, int *i, char *dst)
 {
 	read_space (p, i);
-	if (!is_digit (*p))
-    {
-        return 0;
-    }
+	if (!is_digit (*(p+*i)))
+	{
+		return 0;
+	}
 	while (is_digit (*(p+*i)))
-    {
-    	*dst = *(p+*i);
-        dst = dst + 1;
-        *i = *i + 1;
-    }
-    *dst = 0;
-    return 1;
+	{
+		*dst = *(p+*i);
+		dst = dst + 1;
+		*i = *i + 1;
+	}
+	*dst = 0;
+	return 1;
+}
+
+int read_cstr(char *p, int *i, char *dst)
+{
+	if (!read_sym (p, i, '\"'))
+	{
+		return 0;
+	}
+	*i = *i + 1;
+	while (*(p+*i) != '\"')
+	{
+		if (*(p+*i) == '\\')
+		{
+			*i = *i + 1;
+		}
+		*dst = *(p+*i);
+		dst = dst + 1;
+		*i = *i + 1;
+	}
+	*dst = 0;
+	return 1;
+}
+
+int read_csym(char *p, int *i, char *dst)
+{
+	if (!read_sym (p, i, '\''))
+	{
+		return 0;
+	}
+	*i = *i + 1;
+	while (*(p+*i) != '\'')
+	{
+		if (*(p+*i) == '\\')
+		{
+			*i = *i + 1;
+		}
+		*dst = *(p+*i);
+		dst = dst + 1;
+		*i = *i + 1;
+	}
+	*dst = 0;
+	return 1;
+}
+
+/* Parse and process
+ * functions */
+
+int parse_root(char *p, int *i)
+{
+	char id[16];
+	/* Read identifier as a basis for any
+	 * statement within the program's root.
+	 * It is supposed to be a function declaration */
+	while (read_id (p, i, id))
+	{
+		if (!read_sym (p, i, '('))
+		{
+			break;
+		}
+		// TODO
+	}
+	/* At this moment we always return 1
+	 * with no error checks */
+	return 1;
+}
+
+/* Entry point */
+
+int main()
+{
+	return 0;
 }
